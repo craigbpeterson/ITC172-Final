e0c2d11f88fce92baf3bbff0701220e0b9ca8a17
Steve <tomiolubeko@yahoo.com>
2018-02-26 12:33:01 -0800

    Created Product Controller/Product Index View

---
+++ BakeryApp/BakeryApp/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BakeryApp.Models;
+
+namespace BakeryApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private BakeryEntities db = new BakeryEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult Buy(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product p = db.Products.Find(id);
+            if(p == null)
+            {
+                return HttpNotFound();
+            }
+            return RedirectToAction("Index", "Sale", p);
+        }
+    }
+}
