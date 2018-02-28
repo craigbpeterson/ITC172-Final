6c75b91f8823a28fb6a8cf6d432381961eec321d
Steve <tomiolubeko@yahoo.com>
2018-02-28 12:56:18 -0800

    Added LoginController, RegistrationController

---
+++ BakeryApp/BakeryApp/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryApp.Models;
+
+namespace BakeryApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        BakeryEntities db = new BakeryEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "PersonEmail, PersonPhone")] Person p)
+        {
+            int loginResult = db.usp_PersonLogin(p.PersonEmail, p.PersonPhone);
+
+            if (loginResult != -1)
+            {
+                var uid = (from r in db.People
+                           where r.PersonEmail.Equals(p.PersonEmail)
+                           select r.PersonKey).FirstOrDefault();
+
+                int rKey = (int)uid;
+                Session["PersonKey"] = rKey;
+
+                Message m = new Message();
+                m.MessageText = "Thank you, " + p.PersonFirstName + " for loggin' in." +
+                    "Feel free to browse our tasty selection.";
+
+                return RedirectToAction("Result", m);
+            }
+        }
+    }
+}
---
+++ BakeryApp/BakeryApp/Controllers/RegistrationController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BakeryApp.Models;
+
+namespace BakeryApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        BakeryEntities db = new BakeryEntities();
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "PersonLastName, PersonFirstName, PersonEmail, PersonPhone, PersonDateAdded")] Person dudemanbro)
+        {
+            Person p = new Person();
+            p.PersonLastName = dudemanbro.PersonLastName;
+            p.PersonFirstName = dudemanbro.PersonFirstName;
+            p.PersonEmail = dudemanbro.PersonEmail;
+            p.PersonPhone = dudemanbro.PersonPhone;
+            p.PersonDateAdded = DateTime.Now;
+
+            db.People.Add(p);
+            db.SaveChanges();
+
+            Message m = new Message();
+            m.MessageText = "Thanks for Registering.";
+            return View("Result", m);
+        }
+
+        public ActionResult Result(Message m)
+        {
+            return View(m);
+        }
+
+    }
+}
